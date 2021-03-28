0949181f757760ef004e43539e26f0d035217513
dev-nislam2020 <dev.nislam2020@gmail.com>
2021-03-28 17:32:01 +0600

    some change model budget

--- expense/models.py
+++ expense/models.py
@@ -14,9 +14,26 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
-    
+
+
+class Budget(models.Model):
+    BUDGET_TIME = (
+        ('1','DAY'),
+        ('7','WEEK'),
+        ('30','MONTH'),
+    )
+    name = models.CharField(max_length=50, unique=True)
+    amount = models.IntegerField()
+    budget_for = models.CharField(max_length=50, choices=BUDGET_TIME)
+    deadline = models.DateField()
+    notes = models.TextField(blank=True, null=True)
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+
+
 class Expense(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE, blank=True, null=True)
     amount = models.IntegerField()
     notes = models.TextField(blank=True, null=True)
     create_at = models.DateField()
@@ -28,16 +45,3 @@ class Expense(models.Model):
     def __str__(self):
         return str(self.amount)
 
-class Budget(models.Model):
-    BUDGET_TIME = (
-        ('1','DAY'),
-        ('1','WEEK'),
-        ('1','MONTH'),
-    )
-    name = models.CharField(max_length=50, unique=True)
-    amount = models.IntegerField()
-    budget_for = models.CharField(max_length=50, choices=BUDGET_TIME)
-    deadline = models.DateField()
-    notes = models.TextField(blank=True, null=True)
-    create_at = models.DateField(auto_now_add=True)
-    update_at = models.DateField(auto_now=True)
